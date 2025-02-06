de9ad71cf04a2908a1179b1b37d8da96e3031885
sifat tarafder <tmdsifat98@gmail.com>
2025-02-06 17:58:12 +0600

    some condition added

---
+++ criticalCondition.js
@@ -0,0 +1,11 @@
+let age = 23;
+
+// if( age >= 18 ){
+//     console.log('adult');
+// }
+// else{
+//     console.log('minor');
+// }
+
+let result = age >= 18 ? 'adult' : 'minor';
+console.log(result);
