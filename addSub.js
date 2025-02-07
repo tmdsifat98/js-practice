3a4dd7c7af4dd73da748449dab335d4412ef0dea
sifat tarafder <tmdsifat98@gmail.com>
2025-02-07 18:00:18 +0600

    slice and isArray

--- addSub.js
+++ addSub.js
@@ -2,6 +2,10 @@ const age = [34, 56, 74, 34, 25, 67, 1, 33, 88, 45, 65];
 age.push(88);       //এলিমেন্টকে শেষে যোগ করবে
 console.log( age );
 
+console.log(Array.isArray(age));  // kono variable array ki na seta define kore 
+
+const cut = age.slice(2,8);   //array er moddhe theke kisu element ber kore anar jonne
+console.log(cut);
 
 const marks = [34, 56, 74, 34];
 marks.pop();        //এলিমেন্টকে শেষ থেকে বের করে দেবে
