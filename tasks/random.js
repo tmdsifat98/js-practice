be5a373db93ff24548cae901ec19daeac3c0ee92
sifat tarafder <tmdsifat98@gmail.com>
2025-02-13 23:35:51 +0600

    random interval input variation

--- tasks/random.js
+++ tasks/random.js
@@ -1,8 +1,8 @@
 // Generate a random number between 10 to 20.
 
-function rand() {
-  let result = Math.floor(Math.random() * (20 - 10 + 1) + 10);
+function rand(num1, num2) {
+  let result = Math.floor(Math.random() * (num2 - num1 + 1) + num1);
   return result;
 }
-const output = rand();
-console.log(output);
+const output = rand(10, 20);
+console.log('the random number is', output);
